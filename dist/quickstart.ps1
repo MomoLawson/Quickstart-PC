f2f24b279bb681fcc6e60852d23fe1756f2fb245
MomoLawson <MomoLawson@users.noreply.github.com>
2026-05-03 18:02:14 +0800

    release: v0.86.0

--- dist/quickstart.ps1
+++ dist/quickstart.ps1
@@ -40,7 +40,7 @@ param(
   [switch]$showVersion
 )
 
-$VERSION = "0.85.0"
+$VERSION = "0.86.0"
 $OutputEncoding = [System.Text.Encoding]::UTF8
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $DEFAULT_CFG_URL = "https://raw.githubusercontent.com/MomoLawson/Quickstart-PC/main/config/profiles.json"
